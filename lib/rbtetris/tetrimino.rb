227fceadeecba7e5b4c4f56e107fcd7beaed1253
ChaYoung You <yousbe@gmail.com>
2014-07-18 06:43:25 +0900

    Make `Tetrimino#color` read only

--- lib/rbtetris/tetrimino.rb
+++ lib/rbtetris/tetrimino.rb
@@ -4,7 +4,7 @@ module RbTetris
   # Public: Game pieces for Tetris.
   class Tetrimino
     # Public: Returns the Integer color constant of the Tetrimino.
-    attr_accessor :color
+    attr_reader :color
 
     # Public: Initialize a new Tetrimino. Initialize its color.
     def initialize
